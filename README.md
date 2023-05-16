3e3a03d92d5ec5a8d67451b98793b0e9d8a5dc02
Alexandre Sajus <alexandre.sajus@student-cs.fr>
2023-05-16 04:55:50 +0200

    :tada: Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# Taipy-Dask-Demo
-Querying Big Data using Taipy and Dask
+# Querying Big Data using Taipy and Dask
+
+<p align="center">
+    <img src="media/app-screen.png" alt="WebApp" width="80%" ></img>
+</p>
+This projects focuses on using Taipy to create a Dask pipeline to run queries on a 24Gb dataset of Yelp Reviews and to build a web app to run these queries and display the results.
+
+## Table of Contents
+
+
+## Why Taipy?
+Taipy is an open-source Python library that manages both front and back-end:
+- Taipy GUI helps create web apps quickly using only Python code
+- Taipy Core helps manage data pipelines by visualizing them and caching already computed results
+
+## Why Dask?
+Pandas is a great library when it comes to data analysis, but it is not designed to handle large datasets. Dask is a library that extends Pandas to parallelize computations and handle bigger than memory datasets.
+
+## Datasets
+
+The datasets used in this project are based on Yelp Reviews datasets:
+- `data/yelp_business.csv` contains information about businesses (mainly name and id)
+- `data/yelp_review_repaired_large.csv` is a 24Gb dataset containing Yelp reviews (mainly business id, text and stars)
+
+The goal here will be to join these two datasets and run queries to find average stars and reviews for a specific business.
+
+## Data Pipeline
+
+The Data Pipeline is built using Taipy Studio in VSCode and looks like this:
+
+<p align="center">
+    <img src="media/data-pipeline.png" alt="Pipeline" width="100%" ></img>
+</p>
+
+Blue nodes are **Data Nodes** which store variables or datasets:
+- business_data is the `yelp_business.csv` dataset as a Pandas DataFrame
+- business_dict is a dictionary mapping business ids to business names
+- business_name is the name of the business we want to query
+- business_id is the id of the business we want to query
+- review_path is the path to the `yelp_review_repaired_large.csv` dataset
+- review_data is the dataset as a Dask DataFrame
+- raw_reviews is the reviews that we queried
+- parsed_reviews is raw_reviews but filtered to only contain relevant columns
+
+Between the blue nodes are orange **Task Nodes** which take blue nodes as inputs and return blue nodes as outputs using Python functions.
+
+These **Task Nodes** are called by a green node called **Pipeline Node** which is the entry point of the pipeline.
+- **Task Nodes** have a skippable property which allows them to be skipped if the output was already computed and cached
+- For example, if we already ran a first query and then run a second one, Taipy will log:
+- `[2023-05-16 04:40:06,858][Taipy][INFO] job JOB_read_review_39d2bb45-8901-4081-b877-2e308507bb90 is skipped.`
+- meaning it did not read the dataset again but used the cached result instead.
+
+With this pipeline, we can find reviews for a specific business from the datasets.
+
+## Web App
+
+The web app is built using Taipy GUI and looks like this:
+
+<p align="center">
+    <img src="media/dropdown.png" alt="Dropdown" width="80%" ></img>
+</p>
+
+The app allows you to select a business from a dropdown menu. This will call the pipeline, run the query and display the results: average stars and reviews for the selected business.
+
---
+++ dask_taipy_core.py
@@ -0,0 +1,138 @@
+import taipy as tp
+from taipy import Gui, Config
+import pandas as pd
+import dask.dataframe as dd
+
+BUSINESS_PATH = "data/yelp_business.csv"
+REVIEW_PATH = "data/yelp_review_repaired.csv"
+
+# Load the business data using pandas
+business_df = pd.read_csv(BUSINESS_PATH)
+
+# Create a list of the business names
+selection_names = list(business_df.name[:20])
+
+
+def create_business_dict(business_df: pd.DataFrame):
+    """
+    Creates a dict with the name as key and the business_id as value.
+
+    Args:
+        - business_df: pandas dataframe with the business data
+
+    Returns:
+        - business_dict: dict with the name as key and the business_id as value
+    """
+    business_dict = dict(zip(business_df.name, business_df.business_id))
+    return business_dict
+
+
+def get_data(path_to_csv: str):
+    """
+    Loads a csv file into a dask dataframe.
+    Converts the date column to datetime.
+
+    Args:
+        - path_to_csv: path to the csv file
+
+    Returns:
+        - dataset: dask dataframe
+    """
+    print("Loading data...")
+    dataset = dd.read_csv(path_to_csv)
+    dataset["date"] = dd.to_datetime(dataset["date"])
+    return dataset
+
+
+def get_business_data(business_id: str, data: dd.DataFrame):
+    """
+    Returns a dask dataframe with the reviews of a business.
+
+    Args:
+        - business_id: id of the business
+        - data: dask dataframe with the reviews
+
+    Returns:
+        - df_business: dask dataframe with the reviews of the business
+    """
+    df_business = data[(data.business_id == business_id)].compute()
+    return df_business
+
+
+def parse_business_data(data: dd.DataFrame):
+    """
+    Parses the reviews of a business.
+
+    Args:
+        - data: dask dataframe with the reviews of a business
+
+    Returns:
+        - data: dask dataframe with the parsed reviews
+    """
+    # Sort data by useful
+    data = data.sort_values(by="useful", ascending=False)
+    # Keep only the stars, date and text columns
+    data = data[["stars", "date", "text"]]
+    return data
+
+
+def get_id_from_name(name: str, business_dict: dict):
+    """
+    Returns the business_id from the name of the business.
+
+    Args:
+        - name: name of the business
+        - business_dict: dict with the name as key and the business_id as value
+
+    Returns:
+        - business_id: id of the business
+    """
+    business_id = business_dict[name]
+    return business_id
+
+
+# Taipy Core
+Config.load("config.toml")
+
+scenario_object = Config.scenarios["scenario"]
+
+tp.Core().run()
+scenario = tp.create_scenario(scenario_object)
+tp.submit(scenario)
+
+business_name = '"Dental by Design"'
+reviews = scenario.parsed_reviews.read()
+
+
+def on_selection(state):
+    """
+    Re-runs the scenario when the user selects a business.
+
+    Args:
+        - state: state of the app
+    """
+    print("Running query...")
+    scenario = tp.create_scenario(scenario_object)
+    scenario.business_name.write(state.business_name)
+    tp.submit(scenario)
+    state.reviews = scenario.parsed_reviews.read()
+    print("Query finished")
+
+
+page = """
+# Querying Big Data with Taipy and Dask
+
+## Select a business
+
+<|{business_name}|selector|lov={selection_names}|dropdown|on_change=on_selection|>
+
+## Average stars for that business:
+
+<|{round(np.mean(reviews.stars),2)}|indicator|value={np.mean(reviews.stars)}|min=1|max=5|width=30%|>
+
+## Reviews for that business:
+
+<|{reviews}|table|>
+"""
+
+Gui(page).run()
